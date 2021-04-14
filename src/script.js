5566805f07c88d01b38d03504aca4e39a844c325
Sirfilior <66057362+Sirfilior@users.noreply.github.com>
2021-04-14 21:19:53 +0200

    initial socketio setup

--- src/script.js
+++ src/script.js
@@ -1,1 +1,22 @@
 import "./sass/style.sass";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:5100");
+
+var form = document.getElementById("form");
+var input = document.getElementById("input");
+var messages = document.getElementById("messages");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit("chat message", input.value);
+    input.value = "";
+  }
+});
+
+socket.on("chat message", function (msg) {
+  var item = document.createElement("li");
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
