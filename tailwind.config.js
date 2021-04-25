3235ab9fe63c71fe0656f3f4faceae2979be228e
Sirfilior <66057362+Sirfilior@users.noreply.github.com>
2021-04-26 01:13:50 +0200

    Fix typescript styles purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
+    "./helpers/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
